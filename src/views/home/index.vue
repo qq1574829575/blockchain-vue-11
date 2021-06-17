eb22dc9b9eca349c6431e0650f5d8089b5966409
qq1574829575 <1574829575@qq.com>
2021-06-17 15:03:56 +0800

    1.0

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,10 @@
 <template>
-<div style="margin: 0 auto;width: 80%">
-
+<div style="width: 60%;margin: 20px auto 0;">
+  <div style="display: flex">
+    <i style="font-size: 35px;color: rebeccapurple" class="el-icon-notebook-2"></i>
+    <div style="font-size: 25px;color: rebeccapurple">基于VUE的课程管理系统</div>
+  </div>
+  <el-divider></el-divider>
 </div>
 </template>
 
